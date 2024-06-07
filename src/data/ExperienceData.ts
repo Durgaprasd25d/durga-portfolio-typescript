197c1a943b69e94ed8ec2b28cd0fac638148b1fc
Durgaprasad Dalai <durgaprasaddalai10@gmail.com>
2024-06-07 07:27:15 +0530

    project section completed

--- src/data/ExperienceData.ts
+++ src/data/ExperienceData.ts
@@ -11,11 +11,13 @@ export const ExperienceData: ExperienceDataType[] = [
     details: [
       "Developed a comprehensive management tool for student administration within an educational center, facilitating seamless addition of students and overseeing all related operations, including certificate generation",
       "Implemented dedicated exam sections tailored to facilitate student assessments and academic evaluations",
-      "Utilized HTML, CSS, JavaScript, Node.js, MongoDB, and deployed the application on Heroku",
+      "Implemented security features for students taking exams, including a one-hour timer and login with registration number and password",
+      "Upon login, students can access and take exams, which auto-submit after one hour, providing scores and attendance information",
+      "Utilized HTML, CSS, JavaScript, Node.js, MongoDB, and deployed the application on Hostinger",
     ],
   },
   {
-    title: "Freelance Developer | Paddho Accademy",
+    title: "Freelance Developer | Paddho Academy",
     date: "Feb 2023 - Aug 2023",
     details: [
       "Worked on the Paddho student management project, developing a robust web application accommodating three distinct user roles: students, teachers, and parents",
@@ -28,7 +30,7 @@ export const ExperienceData: ExperienceDataType[] = [
     ],
   },
   {
-    title: "Freelance Developer | Paddho Accademy",
+    title: "Freelance Developer | Paddho Academy",
     date: "Apr 2023 - Jun 2023",
     details: [
       "Contributed to the development of the Paddho employee project, a vital web application for teachers to closely engage with students, fostering seamless communication and facilitating interaction",
@@ -39,13 +41,26 @@ export const ExperienceData: ExperienceDataType[] = [
     ],
   },
   {
-    title: "Freelance Developer | Paddho Accademy",
+    title: "Freelance Developer | Paddho Academy",
     date: "May 2023 - Jul 2023",
     details: [
       "Developed the Paddho students project, a web application available on the Play Store, facilitating online examination attendance, providing instant access to individual scores, and enabling students to gauge their performance relative to their peers",
       "Utilized React.js, PHP, Laravel, and MySQL for development",
       "Project name: Paddho students",
       "Project duration: May 2023 to Jul 2023",
+    ],
+  },
+  {
+    title: "Freelance Developer | Gurukrupa Research and Training Centre",
+    date: "Mar 2024 - Jun 2024",
+    details: [
+      "Developed an admin tool for managing students with features for CRUD operations, image uploading, and exam management",
+      "Admin can create exams, manage CRUD operations with exams, publish exams, and add different questions to different exams",
+      "Implemented security features for students taking exams, including a one-hour timer and login with registration number and password",
+      "Upon login, students can access and take exams, which auto-submit after one hour, providing scores and attendance information",
+      "Utilized React.js, Express, MongoDB, and Node.js for development",
+      "Project name: Gurukrupa Admin",
+      "Project duration: Mar 2024 to Jun 2024",
     ],
   },
 ];
--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,3 +1,10 @@
+import todoListImage from "../images/todoList.png";
+import weatherCheckerImage from "../images/weather.png";
+import paddhoAdmin from "../images/PaddhoAdmin.png";
+import paddhoTeacher from "../images/PaddhoTeacher.png";
+import paddhoStudent from "../images/PaddhoStudent.png";
+import grtcAdmin from "../images/grtc-admin-2.png";
+
 interface ProjectsType {
   title: string;
   description: string;
@@ -10,17 +17,43 @@ export const ProjectsData: ProjectsType[] = [
   {
     title: "React Todo List",
     description: `A simple to-do list app powered by React, Chakra UI, React icons. Save in Local Storage`,
-    image:
-      "https://raw.githubusercontent.com/erfjs/Todo-list/main/readme/example.png",
-    GitHub: "https://github.com/erfjs/Todo-list",
-    hosted: "https://todo-app.erfjs.com",
+    image: todoListImage,
+    GitHub: "https://github.com/Durgaprasd25d/react-todo",
+    hosted: "https://task-tamer.netlify.app/",
   },
   {
     title: "React weather checker",
     description: `A weather checker app powered by React, Vite, TailWind, Swr, PropType, Dark mode`,
-    image:
-      "https://raw.githubusercontent.com/erfjs/weather-checker/main/readme/example.png",
-    GitHub: "https://github.com/erfjs/weather-checker",
-    hosted: `https://weather-checker.erfjs.com`,
+    image: weatherCheckerImage,
+    GitHub: "https://github.com/Durgaprasd25d/weather-checker",
+    hosted: `https://waether-checker-app.netlify.app/`,
+  },
+  {
+    title: "Paddho Admin",
+    description: `A comprehensive admin tool developed for managing educational operations.`,
+    image: paddhoAdmin,
+    GitHub: "https://gitlab.com/spattnaik/mint-student",
+    hosted: `https://paddhoadmin.neverthought.in/`,
+  },
+  {
+    title: "Paddho Teachers",
+    description: `A web application for teachers to closely engage with students, fostering seamless communication and facilitating interaction.`,
+    image: paddhoTeacher,
+    GitHub: "https://gitlab.com/spattnaik/mint-student",
+    hosted: `https://panel.paddho.com/dashboard`,
+  },
+  {
+    title: "Paddho Students",
+    description: `A platform for students to access courses, attend online exams, and view their scores.`,
+    image: paddhoStudent,
+    GitHub: "https://gitlab.com/spattnaik/mint-student",
+    hosted: `https://app.paddho.com/courses`,
+  },
+  {
+    title: "Gurukrupa | Admin",
+    description: `An admin tool for managing students with features for CRUD operations, image uploading, and exam management. It includes exam creation, publishing, and adding questions. Security features include a one-hour timer for exams and login with registration number and password. The student portal allows students to access exams, receive instant scores, and have exams auto-submit after one hour.`,
+    image: grtcAdmin,
+    GitHub: "https://github.com/Durgaprasd25d/gurukrupa-admin",
+    hosted: `https://gurukrupa-admin.netlify.app/`,
   },
 ];
